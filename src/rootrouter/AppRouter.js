d722c2f3742ed107b54b6fe85849fffc9001cbc1
vladnat <vlad27nat10@yandex.by>
2020-09-17 20:33:30 +0300

    'mmm'

---
+++ src/rootrouter/AppRouter.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Login } from '../pages/Login'
+
+export const AppRouter = () => {
+	const Home = () => {
+		<div>home</div>
+	}
+
+	const Register = () => {
+		<div>register</div>
+	}
+
+	const Profile = () => {
+		<div>profile</div>
+	}
+
+	const Header = () => (
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/login'>Login</Link>
+					</li>
+					<li>
+						<Link to='/register'>Register</Link>
+					</li>
+					<li>
+						<Link to='/profile'>Profile</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
+
+	return (
+		<>
+			<Header />
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route path='/login' component={Profile} />
+				<Route path='/register' component={Register} />
+				<Route path='/profile' component={Profile} />
+			</Switch>
+		</>
+	)
+}
