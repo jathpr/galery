d017dd868388687acda31553483916c698ceeebd
Mike <feonop@gmail.com>
2020-09-25 21:08:44 +0300

    add header

---
+++ src/components/Content/Header.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Button, PageHeader, Menu, Dropdown } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
+import Avatar from 'antd/lib/avatar/avatar'
+
+export const HeaderLogo = ({ user }) => {
+	const extra = []
+	const menu = (
+		<Menu className='bcg'>
+			<Menu.Item>
+				<a target='_blank'>показать только мои</a>
+			</Menu.Item>
+			<Menu.Item>
+				<a target='_blank'>профиль</a>
+			</Menu.Item>
+			<Menu.Item>
+				<a target='_blank'>выйти</a>
+			</Menu.Item>
+		</Menu>
+	)
+	if (user) {
+		extra.push(
+			<Dropdown overlay={menu} placement='bottomLeft' arrow>
+				<Button key='3'>Информация</Button>
+			</Dropdown>,
+		)
+		extra.push(
+			<Avatar
+				src='https://st3.depositphotos.com/6707292/16849/i/450/depositphotos_168495402-stock-photo-colorful-peruh-symbol-year-eastern.jpg'
+				size={90}
+				icon={<UserOutlined />}
+			/>,
+		)
+	} else {
+		extra.push(<Button>Войти</Button>)
+	}
+
+	return (
+		<>
+			<PageHeader className='site-page-header' title='Заголовок' extra={extra} />
+		</>
+	)
+}
