8984163b59c491e20a5483468cddd5a57709e4a2
user <silitski2013@mail.ru>
2020-10-06 20:51:15 +0300

    reg

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HeaderLogo } from '../components/Header'
+import { HeaderLogo } from '../Components/Header'
 import { connect } from 'react-redux'
 
 const MainComponent = ({ user }) => (
--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,6 +1,6 @@
 import { SET_USER } from './actionTypes'
 import { authUser } from '../requests/user'
-import {loginUser} from "../firebase";
+import { loginUser, registerUser } from '../firebase'
 
 export const setUser = (user) => ({
 	type: SET_USER,
@@ -8,7 +8,12 @@ export const setUser = (user) => ({
 })
 
 export const authUserThunk = (login, pass) => async (dispatch) => {
-	const responce = await loginUser (login, pass)
+	const responce = await loginUser(login, pass)
 	console.log(responce)
-//	responce.json
+	//	responce.json
+}
+export const registerUserThunk = (login, pass) => async (dispatch) => {
+	const responce = await registerUser(login, pass)
+	console.log(responce)
+	//	responce.json
 }
